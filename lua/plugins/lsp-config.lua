fd5f55ec256a2affd0726568e3aaa996fed2ad36
Krsvsky <krsvski@gmail.com>
2024-05-07 12:35:41 +0300

    first commit

---
+++ lua/plugins/lsp-config.lua
@@ -0,0 +1,58 @@
+return {
+	{
+	"williamboman/mason.nvim",
+	config = function()
+	    require("mason").setup()
+	end
+	},
+	{
+	"williamboman/mason-lspconfig.nvim",
+	config = function()
+		require("mason-lspconfig").setup({
+			ensure_installed = {"lua_ls", "bashls", "clangd", "cssls", "dockerls", "html", "jsonls", "tsserver", "pylsp", "sqlls"}
+		})
+	end
+	},
+	{
+	"neovim/nvim-lspconfig",
+	lazy = false,
+	config = function()
+		local capabilities = require('cmp_nvim_lsp').default_capabilities()
+		local lspconfig = require("lspconfig")
+
+		lspconfig.lua_ls.setup({
+			capabilities = capabilities
+		})
+		lspconfig.bashls.setup({
+			capabilities = capabilities
+		})
+		lspconfig.clangd.setup({
+			capabilities = capabilities
+		})
+		lspconfig.cssls.setup({
+			capabilities = capabilities
+		})
+		lspconfig.dockerls.setup({
+			capabilities = capabilities
+		})
+		lspconfig.html.setup({
+			capabilities = capabilities
+		})
+		lspconfig.jsonls.setup({
+			capabilities = capabilities
+		})
+		lspconfig.tsserver.setup({
+			capabilities = capabilities
+		})
+		lspconfig.pylsp.setup({
+			capabilities = capabilities
+		})
+		lspconfig.sqlls.setup({
+			capabilities = capabilities
+		})
+
+		vim.keymap.set('n', 'K', vim.lsp.buf.hover, {})
+		vim.keymap.set({'n', 'v'}, '<space>ca', vim.lsp.buf.code_action, {})
+	end
+	}
+}
---
+++ lua/plugins/neo-tree.lua
@@ -0,0 +1,15 @@
+return {
+    "nvim-neo-tree/neo-tree.nvim",
+    branch = "v3.x",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
+      "MunifTanjim/nui.nvim",
+    },
+    config = function()
+	vim.keymap.set('n', '<enter>', ':Neotree<CR>')
+	vim.keymap.set('n', '<C-n>', ':Neotree close<CR>')
+    end
+
+}
+
